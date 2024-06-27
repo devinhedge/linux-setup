351e8aefccbde75aa26b392b675bfc50aead94fa
Devin Hedge <217600+devinhedge@users.noreply.github.com>
2024-06-27 09:05:57 -0400

    Create setup.sh
    Commit the skeleton version of the setup.sh script which will update any installed software, deamons, and the OS; and then calls each of the other scripts conditionally based on user input.

---
+++ setup.sh
@@ -0,0 +1,19 @@
+!# /bin/bash
+
+echo "Devin's Debian-based Linux Desktop Setup for Development and Amatuer Radio!"
+
+## Update and Upgrade the machine
+
+sudo apt update && sudo apt full-upgrade -y
+
+## Basic Desktop Communication Tools
+
+## Basic command-line customizations
+
+## Python Development Environment Tools
+
+## Amatuer Radio Operations Tools - Radio/Rig Control
+
+## Amatuer Radio Operations Tools - Digital Modes
+
+## Amatuer Radio Operations Tools - Logging
