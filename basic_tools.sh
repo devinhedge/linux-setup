7700234bfb31c55406a5acfa7675a14d356b2d48
Devin Hedge <217600+devinhedge@users.noreply.github.com>
2024-06-28 10:27:31 -0400

    Update basic_tools.sh
    Updated with applications that come natively in most Debian Linux distribution repositories.

--- basic_tools.sh
+++ basic_tools.sh
@@ -7,16 +7,47 @@
 
 echo "Installing Basic Desktop Communication Tools...\n"
 
-# Signal
-# Telegram
-# Discord
-# OpenOffice
-# Logseq
-# Obsidian
-# OneDrive
-# Dropbox
-# Brave Browser
-# Tor Onion Routing (as a VPN)
-# Outline Client (as a VPN)
-# 1Password desktop client
-# Bitwarden desktop client
+# Install Native Source Repository Application
+
+## Telegram
+sudo apt intall telegram-desktop -y
+
+## LibreOffice
+sudo flatpak install flathub org.libreoffice.LibreOffice
+
+## OneDrive
+sudo apt install onedrive -y
+
+## Dropbox
+sudo apt install dropbox -y
+
+## Tor Onion Routing (as a VPN)
+sudo apt onioncircuits torbrowser-launcher -y
+
+# Install Non-Native Applications requiring downloads from Other Sources or the addition of new repositories
+
+## 1Password desktop client
+
+
+## Bitwarden desktop client
+
+
+## Brave Browser
+
+
+## Discord
+
+
+## Logseq
+
+
+## Obsidian
+
+
+## Outline Client (as a VPN)
+
+
+## Signal
+
+
+echo "Completed Installation of Basic Desktop Communications Tools."
