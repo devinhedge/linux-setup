82c74eebff9b343f5a4ffc6839fc6d27b2ce9766
Devin Hedge <217600+devinhedge@users.noreply.github.com>
2024-06-27 21:57:07 -0400

    Update basic_tools.sh
    Added placeholder comments for each item to install.

--- basic_tools.sh
+++ basic_tools.sh
@@ -1,3 +1,17 @@
 #! /bin/bash
 
 echo "Installing Basic Desktop Communication Tools...\n"
+
+# Signal
+# Telegram
+# Discord
+# OpenOffice
+# Logseq
+# Obsidian
+# OneDrive
+# Dropbox
+# Brave Browser
+# Tor Onion Routing (as a VPN)
+# Outline Client (as a VPN)
+# 1Password desktop client
+# Bitwarden desktop client
