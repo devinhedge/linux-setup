6afc3ffca4fc5921ab88c504db0a94b392dfc87e
Devin Hedge <217600+devinhedge@users.noreply.github.com>
2024-06-27 21:54:34 -0400

    Update setup.sh
    Updated with the call to the script to install the basic desktop communication tools

--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
 !# /bin/bash
+#
+# Author  : Devin Hedge kn4fvh
+# Date    : 27 June 2024
+# Updated : 27 June 2024
+# Purpose : Devin's Debian-based Linux Desktop Setup for Development and Amatuer Radio
+
+exitIfNotRoot
 
 echo "Devin's Debian-based Linux Desktop Setup for Development and Amatuer Radio!"
 
@@ -7,6 +14,15 @@ echo "Devin's Debian-based Linux Desktop Setup for Development and Amatuer Radio
 sudo apt update && sudo apt full-upgrade -y
 
 ## Basic Desktop Communication Tools
+
+while true; do
+    read -p "Do you wish to install Basic Desktop Communication Tools? " yn
+    case $yn in
+        [Yy]* ) ./basic_tools.sh; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
 
 ## Basic command-line customizations
 
